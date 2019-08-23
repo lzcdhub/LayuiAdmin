b42323707813e620c68699637526815d13011298
陈瑞华 <i@chenruihua.com>
2019-08-23 11:30:45 +0800

    更新文件 config.js

--- layuiAdmin.pro-v1.2.1/dist/config.js
+++ layuiAdmin.pro-v1.2.1/dist/config.js
@@ -14,7 +14,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
     ,views: layui.cache.base + 'views/' //视图所在目录
     ,entry: 'index' //默认视图文件名
     ,engine: '.html' //视图文件后缀名
-    ,pageTabs: false //是否开启页面选项卡功能。单页版不推荐开启
+    ,pageTabs: true //是否开启页面选项卡功能。单页版不推荐开启
     
     ,name: 'layuiAdmin Pro'
     ,tableName: 'layuiAdmin' //本地存储表名
