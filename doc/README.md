50a9239fff7dbd3ee1009f0dd8fbe929cd999f34
ChenRuihua <i@chenruihua.com>
2020-03-09 22:50:56 +0800

    20200309闲鱼购买

--- doc/README.md
+++
@@ -1,4 +0,0 @@
-# layuiAdmin pro v1.x 开发者文档
-在线浏览: https://cba.github.io/layuiAdmin-doc/
-
-上传时间:2018-02-06
---
+++ layuiAdmin.pro-v2020.1.24/dist/controller/common.js
@@ -0,0 +1,2 @@
+/** layuiAdmin.pro-v2020.1.24 LPPL License By https://www.layui.com/admin/ */
+ ;layui.define(function(e){var i=(layui.$,layui.layer,layui.laytpl,layui.setter,layui.view,layui.admin);i.events.logout=function(){i.req({url:"./json/user/logout.js",type:"get",data:{},done:function(e){i.exit()}})},e("common",{})});
